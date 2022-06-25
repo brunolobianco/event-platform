d88d4b80f552b2b739be0be315ac9d16e67e867e
Bruno Motta <bruno.l.motta@hotmail.com>
2022-06-25 17:48:58 -0300

    implemented lessons 3 and 4

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4oenfze21a101z7gyhq6imy/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers:{
+        'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+    },
     cache: new InMemoryCache()
 })
